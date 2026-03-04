61296ae9418faeaa56a550868a67bf92e8c8f811
laasyaravi <31567298+laasyaravi@users.noreply.github.com>
2026-03-04 14:24:43 +0530

    Update largest3.sh

--- largest3.sh
+++ largest3.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 
-   if [[ $1 -gt $2 && $1 -gt $3 ]] ;
+   if [ $1 -gt $2 ] && [ $1 -gt $3 ] ;
    then 
        echo " $1 is the greatest"
-   elif [[  $2 -gt $1 && $2 -gt $3 ]] ;
+   elif [  $2 -gt $1 ] && [ $2 -gt $3 ] ;
    then
 
        echo " $2 is the largest"
